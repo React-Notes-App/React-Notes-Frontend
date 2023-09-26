6ab3b180186749a7d9936b6680a61025fd4fc870
Thomas Johnson <123108455+hawkishpolicy@users.noreply.github.com>
2023-09-26 11:48:44 -0400

    testing out netlify deployment

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import NavBar from "./Components/NavBar";
+import "./App.css";
 
 function App() {
+  // const [darkMode, setDarkMode] = useState(false);
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
