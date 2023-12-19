569572aea0a5085ab1632b43d4d5b07daf7b18c3
Thomas Johnson <123108455+hawkishpolicy@users.noreply.github.com>
2023-12-19 12:32:39 -0500

    added or clause to server URL so to make testing easier

--- src/API-Adapter/index.js
+++ src/API-Adapter/index.js
@@ -1,5 +1,4 @@
-// const URL = "https://todo-app-7yv2.onrender.com";
-const URL = "http://localhost:4000";
+const URL = "https://todo-app-7yv2.onrender.com" || "http://localhost:4000";
 
 export { registerCall } from "./registerCall";
 export { loginCall } from "./loginCall";
