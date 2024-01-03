3aca7fa2a1adb6937446134ee9e9c0124f810f99
Thomas Johnson <123108455+hawkishpolicy@users.noreply.github.com>
2024-01-03 16:26:51 -0500

    swapped database url's

--- src/API-Adapter/registerCall.js
+++ src/API-Adapter/registerCall.js
@@ -1,5 +1,5 @@
-// const URL = "https://todo-app-7yv2.onrender.com";
-const URL = "http://localhost:4000";
+const URL = "https://todo-app-7yv2.onrender.com";
+// const URL = "http://localhost:4000";
 
 export const registerCall = async (name, email, password) => {
   try {
